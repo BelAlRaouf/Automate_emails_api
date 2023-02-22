6d402fa27ccd4a8c039b9e81fdb5e3fb7b740bd6
belalraouf <belal.raouf1@gmial.com>
2023-02-22 16:32:52 -0500

    adding some changes to receive the email with subject and description.

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+from send_email import send_email
 
 
 api_key = "890603a55bfa47048e4490069ebee18c"
@@ -9,6 +10,14 @@ request = requests.get(url)
 
 content = request.json()
 
+email_contents = ""
 for article in content["articles"]:
-    print("****", article["title"], "****")
-    print(article["description"])
+    if article['title'] is not None:
+        email_contents = "Subject: Today's News" + "\n"\
+                         + email_contents \
+                         + article["title"] + "\n" \
+                         + article["description"] + "\n"\
+                         + article["url"] + 2*"\n"
+
+email_contents = email_contents.encode("utf-8")
+send_email(message=email_contents)
